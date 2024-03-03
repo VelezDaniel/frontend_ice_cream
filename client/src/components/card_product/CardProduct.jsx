29b4b3a5d6e8673374e730ed836ebe55574daec9
Dvelez <velezdev80@gmail.com>
2024-03-03 17:57:56 -0500

    FrontEnd first commit (copy of a specific commit in fullproject

---
+++ client/src/components/card_product/CardProduct.jsx
@@ -0,0 +1,33 @@
+import './cardproduct.css';
+
+function CardProduct() {
+	return (
+		<div>
+			<div className="card-product">
+				<img
+					className="card-product-img"
+					src="https://www.recetasderechupete.com/wp-content/uploads/2019/07/shutterstock_1010248351.jpg"
+					alt=""
+				/>
+				<div className="card-product-info">
+					<div>
+						<span className="product-name">Nombre Helado</span>
+						<span className="type-product-name">Tipo Helado</span>
+						<div className='div-rank'>
+							<span className='product-name'>4.8</span>
+							<i className="bi bi-star product-name"></i>
+						</div>
+					</div>
+					<div className="product-price">
+						<span>$3500</span>
+						<button className="btn-product-plus">
+							<i className="bi bi-plus-circle-fill plus"></i>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default CardProduct;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import NavBar from "../components/navbar/NavBar";
+import Banner from "../components/banner/banner";
+import MainProducts from "../components/main_products/MainProducts";
+
+function HomePage() {
+
+  return (
+    <div>
+      <NavBar/>
+      <Banner/>
+      <MainProducts/>
+    </div>
+  )
+}
+
+export default HomePage;
