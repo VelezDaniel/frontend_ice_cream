cd30a136499ce9b6c8d7b8eae45e474dffd052fd
Dvelez <velezdev80@gmail.com>
2024-03-18 19:52:12 -0500

    Burguer menu impruved

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,45 +1,98 @@
-import './navbar.css';
-import { useState } from 'react';
+import "./navbar.css";
+import { useState, useEffect } from "react";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 // import { useNavigate } from "react-router-dom";
 // import { Link } from 'react-router-dom';
 
 function NavBar() {
-    // const navigate = useNavigate();
-    const [scroll, setScroll] = useState(false);
+	// const navigate = useNavigate();
+	const [scroll, setScroll] = useState(false);
+	const [menuVisible, setMenuVisible] = useState(false);
 
-    const changeColorNav = () => {
-        if(window.scrollY >= 400) {
-            setScroll(true);
-        } else {
-            setScroll(false);
-        }
-    };
+	const changeColorNav = () => {
+		if (window.scrollY >= 400) {
+			setScroll(true);
+		} else {
+			setScroll(false);
+		}
+	};
 
-    window.addEventListener('scroll', changeColorNav);
+	useEffect(() => {
+		window.addEventListener("scroll", changeColorNav);
+		return () => {
+			window.removeEventListener("scroll", changeColorNav);
+		};
+	}, []); // Agregamos una dependencia vacía para que se ejecute solo una vez al montar el componente
 
-  return (
-    <div>
-      <nav className={`nav-navBar ${scroll ? 'navbar active' : 'navbar'}`}>
-        <div className="open-menu"><i className="fa fa-bars"></i></div>
-        <ul className="main-menu hidden">
-            <li><a href="#">Home</a></li>
-            
-            <li><a href="/products">Productos</a></li>
-            <li><a href="/">Reservar</a></li>
-            <div className="close-menu"><i className="fa fa-times"></i></div>
-        </ul>
-        <div className="logo h-content">
-            <h1>HELARTICO</h1>
-        </div>
+	const openMenu = () => {
+		setMenuVisible(true);
+	};
 
-        <div className="icons h-content">
-            <a className="nav-item nav-icon" href=""><i className="bi bi-handbag"></i></a>
-            <a className="nav-item nav-icon" href="login"><i className="bi bi-person icon-person"></i></a>
-        </div>
-        <div id="cover-sidebar" className="cover-sidebar"></div>
-    </nav>
-    </div>
-  )
+	const closeMenu = () => {
+		setMenuVisible(false);
+	};
+
+	useEffect(() => {
+		const $mainMenu = document.querySelector(".main-menu");
+		const $bgSidebar = document.querySelector("#cover-sidebar");
+		const $hiddenContent = document.querySelector(".h-content");
+
+		if ($mainMenu && $bgSidebar && $hiddenContent) {
+			if (menuVisible) {
+				$mainMenu.style.display = "flex";
+				$bgSidebar.style.display = "block";
+				$hiddenContent.style.pointerEvents = "none";
+				$mainMenu.classList.remove("hidden");
+			} else {
+				$mainMenu.classList.add("hidden");
+				$bgSidebar.style.display = "none";
+				$hiddenContent.style.pointerEvents = "all";
+			}
+		}
+	}, [menuVisible]);
+
+	return (
+		<div>
+			<nav className={`nav-navBar ${scroll ? "navbar active" : "navbar"}`}>
+				<button className="open-menu" onClick={openMenu}>
+					<IoMenu size={32} />
+				</button>
+				<ul className={`main-menu ${menuVisible ? "" : "hidden"}`}>
+					<li>
+						<a href="#">Home</a>
+					</li>
+
+					<li>
+						<a href="/products">Productos</a>
+					</li>
+					<li>
+						<a href="/">Reservar</a>
+					</li>
+					<div className="close-menu" onClick={closeMenu}>
+						<IoClose size={36} />
+					</div>
+				</ul>
+				<div className="logo h-content">
+					<h1>HELARTICO</h1>
+				</div>
+
+				<div className="icons h-content">
+					<a className="nav-item nav-icon" href="">
+						<i className="bi bi-handbag"></i>
+					</a>
+					<a className="nav-item nav-icon" href="login">
+						<i className="bi bi-person icon-person"></i>
+					</a>
+				</div>
+				<div
+					id="cover-sidebar"
+					className={`cover-sidebar ${menuVisible ? "" : "hidden"}`}
+					onClick={closeMenu}
+				></div>
+			</nav>
+		</div>
+	);
 }
 
 export default NavBar;
