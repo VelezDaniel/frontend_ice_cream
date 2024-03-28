c512dda76d1e59c66fa09da376d55f3b513dca83
Dvelez <velezdev80@gmail.com>
2024-03-28 18:05:13 -0500

    design from users almost finished

--- client/src/components/dashboard/dash_users.jsx
+++ client/src/components/dashboard/dash_users.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { showUsers } from "../../api/users";
+import ModalTemplate from "../modal/ModalTemplate";
 import "./css/dash_users.css";
 import { IoSearch } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
@@ -7,19 +8,40 @@ import { HiOutlinePencilAlt, HiOutlineTrash } from "react-icons/hi";
 
 function DashUsers() {
 	const [usersData, setUsersData] = useState([]);
+	const [editModal, setEditModal] = useState(false);
+	const [newUserModal, setNewUSerModal] = useState(false);
+	const [deleteModal, setDeleteModal] = useState(false);
+	const [selectedUserIndex, setSelectedUserIndex] = useState(null);
+
+	const openModalEdit = (index) => {
+		setEditModal(true);
+		setSelectedUserIndex(index);
+	};
+
+	const openModalDelete = (index) => {
+		setDeleteModal(true);
+		setSelectedUserIndex(index);
+	};
+
+	// const openNewUserModal = () => {
+	// 	setNewUSerModalOpen(true);
+	// }
+
+	// const closeModal = () => {
+	// 	setEditModalOpen(false);
+	// };
 
 	useEffect(() => {
 		// Realizar consulta a la base de datos para traer la informacion
 		const handleShowUsers = async () => {
 			try {
 				const items = await showUsers();
-				console.log(items.data.body);
 				// Establecer usuarios en estado
 				setUsersData(items.data.body);
 			} catch (error) {
-				console.log('Error in dash_users: ',error);
+				console.log("Error in dash_users: ", error);
 			}
-		}
+		};
 		handleShowUsers();
 	}, []);
 
@@ -31,55 +53,100 @@ function DashUsers() {
 					<input type="text" placeholder="Buscar por Identificacion" />
 				</div>
 			</div>
-			<div>
-				<button>
-					<FaRegUser />
+			<div className="div-btn-add-user">
+				<button onClick={() => setNewUSerModal(!newUserModal)}>
+					<FaRegUser size={20} />
 					Agregar
 				</button>
 			</div>
+			{/* MODAL AÑADIR USUARIO */}
+			{newUserModal && (
+				<ModalTemplate
+					setStateModal={setNewUSerModal}
+					title="Nuevo Usuario"
+					showHeader={true}
+				>
+					<p>New user</p>
+				</ModalTemplate>
+			)}
+			{/* Contenedor de cada fila */}
 			<div className="dash-container-cards">
-				{usersData.map((user, index) => (
+				{usersData.map((userData, index) => (
 					<div className="dash-container-card" key={index}>
 						<div className="dash-card-user">
 							<div className="colum-one">
-								<span>{user.name.concat(` ${user.lastName}`)}</span>
-								<span>{user.role}</span>
+								<span>{userData.name.concat(` ${userData.lastName}`)}</span>
+								<span>{userData.role}</span>
 							</div>
 							<div className="colum-two">
 								<div>
 									<span>Identificacion</span>
-									<span>{user.identity}</span>
+									<span>{userData.identity}</span>
 								</div>
 								<div>
 									<span>Celular</span>
-									<span>{user.phone}</span>
+									<span>{userData.phone}</span>
 								</div>
 								<div>
 									<span>Correo</span>
-									<span>{user.email}</span>
+									<span>{userData.email}</span>
 								</div>
 								<div>
 									<span>Dirección</span>
-									<span>{user.address}</span>
+									<span>{userData.address}</span>
 								</div>
 								<div>
 									<span>Estado</span>
-									<span>{user.state}</span>
+									<span>{userData.state}</span>
 								</div>
 								<div>
 									<span> </span>
-									<span>{user.birth}</span>
+									<span>{userData.birth}</span>
 								</div>
 							</div>
 						</div>
+
 						<div className="dash-container-btns">
-							<button>
-								<HiOutlinePencilAlt />
+							<button
+								className="dash-btn-edit"
+								onClick={() => openModalEdit(index)}
+							>
+								<HiOutlinePencilAlt size={38} />
 							</button>
-							<button>
-								<HiOutlineTrash />
+							<button
+								className="dash-btn-delete"
+								onClick={() => openModalDelete(index)}
+							>
+								<HiOutlineTrash size={38} />
 							</button>
 						</div>
+
+						{/* mostrar modal editar */}
+						{editModal && selectedUserIndex === index && (
+							<ModalTemplate
+								setStateModal={setEditModal}
+								title="Editar Usuario"
+								showHeader={true}
+								designClass={""}
+							>
+								<p>Ponete a funcionar</p>
+								<p>{userData.name}</p>
+								<p>{userData.lastName}</p>
+							</ModalTemplate>
+						)}
+						{/* Mostrar modal eliminar */}
+						{deleteModal && selectedUserIndex === index && (
+							<ModalTemplate
+								setStateModal={setDeleteModal}
+								title={" Eliminar Usuario "}
+								showHeader={true}
+								designClass={"alert"}
+							>
+								<div>
+									<span>{userData.email}</span>
+								</div>
+							</ModalTemplate>
+						)}
 					</div>
 				))}
 			</div>
---
+++ client/src/components/modal/ModalTemplate.jsx
@@ -0,0 +1,40 @@
+import "./modal_template.css";
+import { IoIosClose } from "react-icons/io";
+
+const ModalTemplate = ({
+	modalContent,
+	children,
+	setStateModal,
+	title,
+	showHeader,
+	designClass,
+}) => {
+	return (
+		<div className="cover-modal">
+			<div className="container-modal" isOpen={true}>
+				{showHeader && (
+					<div
+						className={`modal-header ${
+							designClass === "alert" ? "modal-alert-color" : "modal-info-color"
+						}`}
+					>
+						<h3>{title}</h3>
+					</div>
+				)}
+				<button
+					className="btn-close-modal"
+					onClick={() => setStateModal(false)}
+				>
+					<IoIosClose size={34} />
+				</button>
+				{modalContent ? (
+					<div className="modal-content">{modalContent}</div>
+				) : (
+					<div className="modal-content">{children}</div>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default ModalTemplate;
