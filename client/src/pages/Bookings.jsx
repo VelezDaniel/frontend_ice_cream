9fac7d9131b5c1ca818fc39e08a1b11767a08bb7
Dvelez <velezdev80@gmail.com>
2024-05-05 13:59:50 -0500

    Try to fixed and improve bookings

--- client/src/pages/Bookings.jsx
+++ client/src/pages/Bookings.jsx
@@ -2,12 +2,15 @@ import NavBar from "../components/navbar/NavBar";
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useForm } from "react-hook-form";
+import { createBookingRequest } from "../api/bookings";
 
 // primereact
 import { Calendar } from "primereact/calendar";
 import { InputTextarea } from "primereact/inputtextarea";
 import { FloatLabel } from "primereact/floatlabel";
 import { InputText } from "primereact/inputtext";
+
+import { Message } from "primereact/message";
 
 // styles
 import "../css/bookings.css";
@@ -19,20 +22,85 @@ import { FaRegAddressCard } from "react-icons/fa";
 import { FiClock } from "react-icons/fi";
 
 function Bookings() {
-	const { register, handleSubmit, errors } = useForm();
-
 	const { user } = useAuth();
-	// const [datetime24h, setDateTime24h] = useState(null);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 	const [date, setDate] = useState(null);
-	const [valueText, setValueText] = useState("");
-	const [textUser, setTextUser] = useState("");
+	const [time, setTime] = useState(null);
+	const [client, setClient] = useState("");
+	const [ident, setIdent] = useState("");
+	const [customErrors, setCustomErrors] = useState({});
 
-	const handlerCalendarChanger = (e) => {
-		setDateTime24h(e.value);
+	const formatDate = (date) => {
+		const year = date.getFullYear();
+		const month = (date.getMonth() + 1).toString().padStart(2, "0");
+		const day = date.getDate().toString().padStart(2, "0");
+		return `${year}-${month}-${day}`;
 	};
 
-	const onSubmit = (data) => {
-		console.log(data);
+	const formatTime = (time) => {
+		const hours = time.getHours().toString().padStart(2, "0");
+		const minutes = time.getMinutes().toString().padStart(2, "0");
+		const seconds = time.getSeconds().toString().padStart(2, "0");
+		return `${hours}:${minutes}:${seconds}`;
+	};
+
+	const handlerClient = (e) => {
+		setClient(e.target.value);
+	};
+
+	const handlerIdent = (e) => {
+		setIdent(e.target.value);
+	};
+
+	const clientValid = () => {
+		if (!client || !ident) {
+			return false;
+		}
+		return true;
+	};
+
+	const onSubmit = async (data) => {
+		if (!date || !time) {
+			setCustomErrors((prevErrors) => ({
+				...prevErrors,
+				addDate: "Fecha es requerida",
+			}));
+			return;
+		}
+
+		let info;
+		// e.preventDefault();
+		const dateTime =
+			data && time ? formatDate(date) + " " + formatTime(time) : null;
+		console.log(dateTime); // Imprime la fecha y la hora combinadas
+		console.log(data); // Imprime todos los datos del formulario
+		console.log("user book: ", user);
+		if (!user) {
+			info = {
+				id: 0,
+				dateBook: dateTime,
+				description: data.addDescription,
+				attendees: data.addGuests,
+				hiddenDescription: `Identificación: ${ident} | Nombre: ${client}`,
+			};
+		}
+		if (user) {
+			info = {
+				id: 0,
+				dateBook: dateTime,
+				description: data.addDescription,
+				attendees: data.addGuests,
+				idClient: user.id,
+			};
+		}
+
+		const addBook = await createBookingRequest(info);
+		console.log("addBook response: ", addBook);
+		// location.reload();
 	};
 
 	return (
@@ -61,47 +129,53 @@ function Bookings() {
 								</label>
 
 								<Calendar
-									{...register("addDate", {
-										required: {
-											value: true,
-											message: "Campo requerido",
-										},
-									})}
+									// {...register("addDate", {
+									// 	required: {
+									// 		value: true,
+									// 		message: "Campo requerido",
+									// 	},
+									// })}
+									required
 									className="calendar-book"
 									value={date}
 									onChange={(e) => setDate(e.value)}
 									showIcon
 								/>
 							</div>
-							{errors && errors.addDate && (
-								<p className="notice">{errors.addDate.message}</p>
+							{customErrors && customErrors.addDate && (
+								// <p className="notice">{errors.addDate.message}</p>
+								<div className="card flex justify-content-center">
+									<Message text="Username is required" />
+								</div>
 							)}
 							<div className="flex-auto">
 								<label
-									htmlFor="buttondisplay"
+									htmlFor="calendar-timeonly"
 									className="font-bold block mb-2 label-date-book"
 								>
 									Elige la hora de tu evento
 								</label>
 
 								<Calendar
-									{...register("addTime", {
-										required: {
-											value: true,
-											message: "Campo requerido",
-										},
-									})}
-									className="calendar-book"
-									value={date}
-									onChange={(e) => setDate(e.value)}
+									// {...register("addTime", {
+									// 	required: {
+									// 		value: true,
+									// 		message: "Campo requerido",
+									// 	},
+									// })}
+									required
+									id="calendar-timeonly"
+									className="font-bold block mb-2 calendar-book"
+									value={time}
+									onChange={(e) => setTime(e.value)}
 									showIcon
 									timeOnly
 									icon={() => <FiClock />}
 								/>
 							</div>
-							{errors && errors.addTime && (
+							{/* {errors && errors.addTime && (
 								<p className="notice">{errors.addTime.message}</p>
-							)}
+							)} */}
 						</div>
 						<div className="card flex justify-content-center div-calendar-book">
 							<LuBookMarked size={54} />
@@ -112,11 +186,12 @@ function Bookings() {
 								<FloatLabel>
 									<InputTextarea
 										id="description"
-										value={valueText}
-										onChange={(e) => setValueText(e.target.value)}
+										// value={valueText}
+										// onChange={(e) => setValueText(e.target.value)}
 										rows={4}
 										cols={40}
-										name="addDescription"
+										// name="addDescription"
+										required
 										{...register("addDescription", {
 											required: {
 												value: true,
@@ -145,6 +220,7 @@ function Bookings() {
 										placeholder="4"
 										className="input-int-form"
 										name="addGuests"
+										required
 										{...register("addGuests", {
 											required: {
 												value: true,
@@ -163,9 +239,6 @@ function Bookings() {
 									/>
 								</div>
 							</div>
-							{errors && errors.addGuests && (
-								<p className="notice">{errors.addGuests.message}</p>
-							)}
 						</div>
 						{!user && (
 							<div className="div-container-calendar div-space">
@@ -178,45 +251,47 @@ function Bookings() {
 										<FaRegUser size={38} />
 										<FloatLabel>
 											<InputText
+												required
 												className="calendar-book"
-												id="username"
-												value={textUser}
-												onChange={(e) => setTextUser(e.target.value)}
-												name="addClient"
-												{...register("addClient", {
-													required: {
-														value: true,
-														message: "Campo requerido",
-													},
-												})}
+												id="client"
+												value={client}
+												onChange={(e) => setClient(e.target.value)}
+												// name="addClient"
+												// {...register("addClient", {
+												// 	required: {
+												// 		value: true,
+												// 		message: "Campo requerido",
+												// 	},
+												// })}
 											/>
 											<label className="inside-label-input" htmlFor="username">
 												Tu Nombre Completo
 											</label>
 										</FloatLabel>
 									</div>
-									{errors && errors.addClient && (
+									{/* {errors && errors.addClient && (
 										<p className="notice">{errors.addClient.message}</p>
-									)}
+									)} */}
 									<div className="card flex justify-content-center div-calendar-book">
 										<FaRegAddressCard size={38} />
 										<FloatLabel>
 											<InputText
+												required
 												className="calendar-book"
 												id="userIdentity"
-												value={textUser}
-												onChange={(e) => setTextUser(e.target.value)}
-												name="userIdentity"
-												{...register("addIdentity", {
-													required: {
-														value: true,
-														message: "Campo requerido",
-													},
-													minLength: {
-														value: 6,
-														message: "No es una identificación válida",
-													},
-												})}
+												value={ident}
+												onChange={(e) => setIdent(e.target.value)}
+												// name="userIdentity"
+												// {...register("addIdentity", {
+												// 	required: {
+												// 		value: true,
+												// 		message: "Campo requerido",
+												// 	},
+												// 	minLength: {
+												// 		value: 6,
+												// 		message: "No es una identificación válida",
+												// 	},
+												// })}
 											/>
 											<label className="inside-label-input" htmlFor="username">
 												Número de Identificacion
@@ -224,9 +299,9 @@ function Bookings() {
 										</FloatLabel>
 									</div>
 								</div>
-								{errors && errors.userIdentity && (
+								{/* {errors && errors.userIdentity && (
 									<p className="notice">{errors.addIdentity.message}</p>
-								)}
+								)} */}
 							</div>
 						)}
 						<div className="container-btn">
