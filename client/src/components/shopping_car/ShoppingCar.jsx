0832fb49d8fa149b9f784dcbb843d1a9a8cba3bb
Dvelez <velezdev80@gmail.com>
2024-05-29 22:21:32 -0500

    working in cart 2nd part (almost finished)

--- client/src/components/shopping_car/ShoppingCar.jsx
+++ client/src/components/shopping_car/ShoppingCar.jsx
@@ -4,6 +4,13 @@ import { CartContext } from "../../context/ShoppingCartContext";
 import { useContext } from "react";
 import ProductImgBuilder from "../../utils/ProductImgBuilder";
 
+// * MATERIAL UI IMPORTS
+import IconButton from "@mui/material/IconButton";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DriveFileRenameOutlineOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import RemoveOutlinedIcon from "@mui/icons-material/RemoveOutlined";
+
 function ShoppingCar({ closeMethod }) {
 	const [cart, setCart] = useContext(CartContext);
 
@@ -16,6 +23,65 @@ function ShoppingCar({ closeMethod }) {
 		(accumulator, current) => accumulator + current.quantity * current.price,
 		0
 	);
+
+	// ? FUNCIONES PREVIAS NECESARIAS PARA EL CARRITO
+	const addToCart = (id, orderBody, price) => {
+		setCart((currentItems) => {
+			const isItemsFound = currentItems.find((item) => item.id === id);
+			if (isItemsFound) {
+				return currentItems.map((item) => {
+					if (item.id === id) {
+						return { ...item, quantity: item.quantity + 1 };
+					} else {
+						return item;
+					}
+				});
+			} else {
+				return [...currentItems, { id, quantity: 1, price, orderBody }];
+			}
+		});
+	};
+
+	// // ? Añadir al carrito un producto
+	// const addToCart = () => {
+	// 	setCart((currentItems) => {
+	// 		const isItemsFound = currentItems.find((item) => item.id === id);
+	// 		if (isItemsFound) {
+	// 			return currentItems.map((item) => {
+	// 				if (item.id === id) {
+	// 					return { ...item, quantity: item.quantity + 1 };
+	// 				} else {
+	// 					return item;
+	// 				}
+	// 			});
+	// 		} else {
+	// 			return [...currentItems, { id, quantity: 1, price }];
+	// 		}
+	// 	});
+	// };
+
+	// ? Eliminar un producto del carrito (uno por uno)
+	const removeItemCart = (id) => {
+		setCart((currentItems) => {
+			if (currentItems.find((item) => item.id === id)?.quantity === 1) {
+				return currentItems.filter((item) => item.id !== id);
+			} else {
+				return currentItems.map((item) => {
+					if (item.id === id) {
+						return { ...item, quantity: item.quantity - 1 };
+					} else {
+						return item;
+					}
+				});
+			}
+		});
+	};
+
+	// ? Eliminar completamente el producto sin importar si cantidad es mayor a 1
+	const removeAllProduct = (id) => {
+		const updateCart = cart.filter((item) => item.id !== id);
+		setCart(updateCart);
+	};
 
 	return (
 		<div className="car-container">
@@ -35,7 +101,7 @@ function ShoppingCar({ closeMethod }) {
 						<h4 className="car-subtitle">Lista de productos</h4>
 					</div>
 					<div className="divider">
-						<div>
+						<div className="container-prodcuts">
 							{cart.map((order) => (
 								<div className="small-card-cart" key={order.id}>
 									<div className="box-img-card-product">
@@ -71,17 +137,65 @@ function ShoppingCar({ closeMethod }) {
 										) : (
 											<p className="product-size-cart">Sin adiciones</p>
 										)}
+
 										<div className="cart-sect-horiontal top-space-smallest">
 											<p>Total Producto</p>
 											<p className="total-product-order">${order.price}</p>
+										</div>
+
+										<div className="container-icons-product-cart">
+											{/* Edit button */}
+											<IconButton sx={{ width: 42, height: 42 }}>
+												<DeleteOutlineOutlinedIcon
+													color="primary"
+													sx={{
+														width: 30,
+														height: 30,
+													}}
+												/>
+											</IconButton>
+											{/* Plus button */}
+											<IconButton onClick={() => addToCart(order.id, order.orderBody, order.price)} sx={{ width: 42, height: 42 }}>
+												<AddOutlinedIcon
+													color="primary"
+													sx={{
+														width: 30,
+														height: 30,
+													}}
+												/>
+											</IconButton>
+											<span className="amount-product">{order.quantity}</span>
+											{/* Minus Button */}
+											<IconButton onClick={() => removeItemCart(order.id)} sx={{ width: 42, height: 42 }}>
+												<RemoveOutlinedIcon
+													color="primary"
+													sx={{
+														width: 30,
+														height: 30,
+													}}
+												/>
+											</IconButton>
+											{/* Delete button */}
+											<IconButton
+												onClick={() => removeAllProduct(order.id)}
+												sx={{ width: 42, height: 42 }}
+											>
+												<DeleteOutlinedIcon
+													color="primary"
+													sx={{
+														width: 30,
+														height: 30,
+													}}
+												/>
+											</IconButton>
 										</div>
 									</div>
 								</div>
 							))}
 						</div>
-						<div>
-							<div>Items in car: {quantity}</div>
-							<div>Total: ${totalPrice}</div>
+						<div className="container-products-cart-price top-space">
+							<p>Productos: {quantity}</p>
+							<p>Total: ${totalPrice}</p>
 						</div>
 					</div>
 				</div>
