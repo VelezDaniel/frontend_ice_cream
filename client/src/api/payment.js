a7cf6b2e19ecec64670356e13dc0427cdf77456d
Dvelez <velezdev80@gmail.com>
2024-06-08 14:17:18 -0500

    working in payment (reset commit next step)

--- client/src/api/payment.js
+++ client/src/api/payment.js
@@ -1,3 +1,5 @@
 import axios from './axios';
 
 export const createOrderRequest = async (orderInfo) => axios.post('/payment/create-order', orderInfo);
+
+export const captureOrderRequest = async (token) => axios.get(`/payment/capture-order?token=${token}`);
--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -1,11 +1,41 @@
 import { useEffect, useState } from "react";
 import ModalTemplate from "../components/modal/ModalTemplate";
 import "../css/payed.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation} from "react-router-dom";
+import { captureOrderRequest } from "../api/payment";
+// react icons
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 const Payed = () => {
 	const [closeModal, setCloseModal] = useState(true);
+	const [stateCancelModal, setStateCancelModal] = useState(false);
+	const [stateSuccesModal, setStateSuccesModal] = useState(false);
+
 	const navigate = useNavigate();
+	const location = useLocation();
+	// const history = useHistory();
+
+	useEffect(() => {
+		const confirmPayment = async () => {
+			const params = new URLSearchParams(location.search);
+			const token = params.get("token");
+
+			try {
+				const response = await captureOrderRequest(token);
+
+				if (response.data === "payed") {
+					console.log("Payment confirmed: ", response.data);
+					setStateSuccesModal(true);
+				} else {
+					console.log("Payment failed: ", response.data);
+				}
+			} catch (error) {
+				console.log("error in confirm payment: ", error);
+			}
+		};
+
+		confirmPayment();
+	}, [location]);
 
 	useEffect(() => {
 		if (closeModal === false) {
@@ -13,24 +43,60 @@ const Payed = () => {
 		}
 	}, [closeModal]);
 
+	// const successEvent = () => {
+
+	// };
+
+	// const cancelEvent = () => {
+
+	// };
+
 	return (
 		<div className="payed-bg">
-			<ModalTemplate bg={"bg-blue-light"} setStateModal={setCloseModal}>
-				<div className="body-modal-payment">
-					<div className="loader"></div>
-					<div>
-						<h4 className="title-modal-payment">
-							¡Ya puedes saborear ese exquisito helado!
-						</h4>
-						<p className="description-modal-payment">
-							Muy pronto recibirás tus productos
-						</p>
-						<button onClick={() => setCloseModal(false)} className="btn-bright">
-							Regresar
-						</button>
+			<div className="loader">
+				<span className="loader-text">Estamos procesando tu pago...</span>
+			</div>
+			{stateSuccesModal && stateSuccesModal === true ? (
+				<ModalTemplate bg={"bg-blue-light"} setStateModal={setCloseModal}>
+					<div className="body-modal-payment">
+						<div className="loader"></div>
+						<div>
+							<h4 className="title-modal-payment">
+								¡Ya puedes saborear ese exquisito helado!
+							</h4>
+							<p className="description-modal-payment">
+								Muy pronto recibirás tus productos
+							</p>
+							<button
+								onClick={() => setStateSuccesModal(true)}
+								className="btn-bright"
+							>
+								Regresar
+							</button>
+						</div>
 					</div>
-				</div>
-			</ModalTemplate>
+				</ModalTemplate>
+			) : (
+				<ModalTemplate bg={"bg-red-light"} setStateModal={setCloseModal}>
+					<div className="body-modal-payment">
+						<IoIosCloseCircleOutline />
+						<div>
+							<h4 className="title-modal-payment">
+								¡Oooh Nooo! Parece que algo salio mal.
+							</h4>
+							<p className="description-modal-payment">
+								Vuelve a intentar hacer el pago y no te quedes sin tus productos
+							</p>
+							<button
+								onClick={() => setCloseModal(false)}
+								className="btn-bright"
+							>
+								Regresar
+							</button>
+						</div>
+					</div>
+				</ModalTemplate>
+			)}
 		</div>
 	);
 };
